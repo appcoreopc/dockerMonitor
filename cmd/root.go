25d0a5202b02acafc5dca53c36432378c9e330ba
jeremy woo <kepungmoin@hotmail.com>
2019-02-25 21:44:48 +1300

    added cobra feature.

---
+++ cmd/root.go
@@ -0,0 +1,67 @@
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var Verbose bool
+
+// rootCmd represents the root command
+var rootCmd = &cobra.Command{
+	Use:              "root",
+	TraverseChildren: true,
+	Short:            "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func init() {
+
+	//rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+	//rootCmd.MarkFlagRequired("verbose")
+
+	//viper.SetConfigName("config") // name of config file (without extension)
+	//viper.AddConfigPath(".")      // optionally look for config in the working directory
+	//err := viper.ReadInConfig()   // Find and read the config file
+	// if err != nil { // Handle errors reading the config file
+	// 	panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	// }
+
+	// viper.AddRemoteProvider("etcd", "http://127.0.0.1:2379", "v2/keys/settings")
+	// viper.SetConfigType("json") // because there is no file extension in a stream of bytes, supported extensions are "json", "toml", "yaml", "yml", "properties", "props", "prop"
+	// viper.SetConfigName("settings")
+	// viper.ReadRemoteConfig()
+}
+
+func Execute() {
+
+	fmt.Println("testing testing")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/appcoreopc/dockerMonitor/cmd"
+
+func main() {
+
+	cmd.Execute()
+
+}
